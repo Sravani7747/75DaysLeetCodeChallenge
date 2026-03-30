a13e80650335e139b3e56e734b6dff809a878433
sravani <324103382052@gvpce.ac.in>
2026-03-30 22:22:47 +0530

    Time: 1 ms (67.69%), Space: 9.5 MB (16.92%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.c
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.c
@@ -1,21 +1,21 @@
 int characterReplacement(char* s, int k) {
     int freq[26] = {0};
-    int left = 0, right = 0;
+    int l = 0, r = 0;
     int maxFreq = 0;
     int maxLen = 0;
-    while (s[right] != '\0') {
-        freq[s[right] - 'A']++;
-        if (freq[s[right] - 'A'] > maxFreq) {
-            maxFreq = freq[s[right] - 'A'];
+    while (s[r] != '\0') {
+        freq[s[r] - 'A']++;
+        if (freq[s[r] - 'A'] > maxFreq) {
+            maxFreq = freq[s[r] - 'A'];
         }
-        while ((right - left + 1) - maxFreq > k) {
-            freq[s[left] - 'A']--;
-            left++;
+        while ((r - l + 1) - maxFreq > k) {
+            freq[s[l] - 'A']--;
+            l++;
         }
-        if ((right - left + 1) > maxLen) {
-            maxLen = right - left + 1;
+        if ((r - l + 1) > maxLen) {
+            maxLen = r - l + 1;
         }
-        right++;
+        r++;
     }
     return maxLen;
 }
