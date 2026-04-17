32a63b636b8b6772e63bd5c019f490a36917334c
sravani <324103382052@gvpce.ac.in>
2026-04-17 22:28:29 +0530

    Time: 3 ms (22.95%), Space: 15 MB (67.62%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.c
@@ -0,0 +1,18 @@
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (p == NULL && q == NULL)
+        return true;
+    if (p == NULL || q == NULL)
+        return false;
+    return (p->val == q->val) &&
+           isSameTree(p->left, q->left) &&
+           isSameTree(p->right, q->right);
+}
+bool isSubtree(struct TreeNode* root, struct TreeNode* subRoot) {
+    if (root == NULL)
+        return false;
+    if (isSameTree(root, subRoot))
+        return true;
+    return isSubtree(root->left, subRoot) ||
+           isSubtree(root->right, subRoot);
+}
