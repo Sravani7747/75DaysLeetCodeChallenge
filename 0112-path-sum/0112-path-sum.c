8c54ec082fdae72b00191c12cdf59b4784fac839
sravani <324103382052@gvpce.ac.in>
2026-04-18 16:46:57 +0530

    Time: 0 ms (100%), Space: 12.2 MB (27.65%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.c
@@ -0,0 +1,12 @@
+
+bool hasPathSum(struct TreeNode* root, int targetSum) {
+    if (root == NULL) {
+        return false;
+    }
+    if (root->left == NULL && root->right == NULL) {
+        return (targetSum == root->val);
+    }
+    int remainingSum = targetSum - root->val;
+    return hasPathSum(root->left, remainingSum) || 
+           hasPathSum(root->right, remainingSum);
+}
