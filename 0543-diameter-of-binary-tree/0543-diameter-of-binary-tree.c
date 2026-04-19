2bba6f888f3bb71a944bd76b190b1dff825eb83f
sravani <324103382052@gvpce.ac.in>
2026-04-19 22:10:53 +0530

    Time: 0 ms (100%), Space: 14.7 MB (17.44%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.c
@@ -0,0 +1,17 @@
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+int height(struct TreeNode* root, int* diameter) {
+    if (root == NULL)
+        return 0;
+    int left = height(root->left, diameter);
+    int right = height(root->right, diameter);
+    *diameter = max(*diameter, left + right);
+    return 1 + max(left, right);
+}
+int diameterOfBinaryTree(struct TreeNode* root) {
+    int diameter = 0;
+    height(root, &diameter);
+    return diameter;
+}
